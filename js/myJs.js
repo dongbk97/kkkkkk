f883d383ee3002c814a324f987998b1981f45a3e
dongbk97 <dongtr2202@gmail.com>
2022-10-22 16:33:05 +0700

    dfg

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
   text1: "Hi Cảnh !",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Cảnh ................",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text2: "Có cái ni Cảnh thử nhá",
+  text3: "Cảnh có muốn thử hay không ._.",
+  text4: "Không được thoát nhé :v",
+  text5: "Không thèm",
+  text6: "Thử ở đây này <3",
+  text7: "Cảnh viết cái chi vô đây đi",
+  text8: "Gửi này",
+  text9: "Cảnh muốn nói ...............",
+  text10: "Haha nghe kiểu con nít v:",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Mạn vài bức ảnh nha",
+  text12: "Vô đây để coi nầy",
 };
 
 $(document).ready(function () {
